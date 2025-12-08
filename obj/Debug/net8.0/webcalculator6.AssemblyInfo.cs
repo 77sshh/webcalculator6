7ec935fda55fb9ca20aeefbbe674b520d2f2742e
77sshh <77headshotpercentage@gmail.com>
2025-12-08 11:14:25 +0500

    исправление под кафедру лаба 2

--- obj/Debug/net8.0/webcalculator6.AssemblyInfo.cs
+++ obj/Debug/net8.0/webcalculator6.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webcalculator6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebe27e96348001f62230614ef0f8a70b8cc02451")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bb5962ceb6449d03d4ecbb766b7b65c02f4dbcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("webcalculator6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webcalculator6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
